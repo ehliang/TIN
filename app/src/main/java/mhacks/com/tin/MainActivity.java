8b119c4d809cc0fdc4ead9c4d687491867eba99a
Ethan Liang <ehliang@uwaterloo.ca>
2016-02-20 04:28:57 -0500

    Location tracking on button click added

--- app/src/main/java/mhacks/com/tin/MainActivity.java
+++ app/src/main/java/mhacks/com/tin/MainActivity.java
@@ -1,9 +1,17 @@
 package mhacks.com.tin;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +19,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+
+        final LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if (location!=null)
+                {
+                    locationManager.removeUpdates(this);
+                }
+                double lat = location.getLatitude();
+                double longi = location.getLongitude();
+                TextView test = (TextView) findViewById(R.id.test_text);
+                test.setText("" + lat + longi);
+
+                Log.i(String.valueOf(lat), String.valueOf(longi));
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                Log.i("onstatus", "changed");
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+                Log.i("provider", "enabled");
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+                Log.i("provider", "disabled");
+            }
+        };
+
+        Button sendLocation = (Button) findViewById(R.id.send_location);
+        sendLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("pressed", "button");
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+
+            }
+        });
+
+
+
     }
 
     @Override
