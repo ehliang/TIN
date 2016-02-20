ecafcec9a53d9a0a10b7dd075068bd5994dd7ca4
Qile Wang <wangqile123@gmail.com>
2016-02-20 04:35:47 -0500

    Added a way to test the twilio api

---
+++ twilio-demo/newapp.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var express = require('express');
+var twilio = require('twilio');
+var bodyParser = require('body-parser');
+
+var app = express();
+//var mapRouter = require('../routes/mapRouter.js');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// middleware to use for all requests
+// set the response headers
+app.use(function(req, res, next) {
+    // do logging
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  	res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Save-Data");
+  	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  	res.setHeader('Access-Control-Allow-Credentials', false);
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+// choose whatever port we want to listen on
+var port = process.env.PORT || 8080;
+
+app.post('/', function(req, res){
+    console.dir(String(req.body.Body));
+    res.send("test");
+}); 
+
+app.listen(port);
