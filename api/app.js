60cd3652f577e4ff53f7127e31d85fa7702e7657
cspanda <robbin.xu@gmail.com>
2016-02-20 03:35:21 -0500

    fixing module.exports in mapRouters.js

--- api/app.js
+++ api/app.js
@@ -1,6 +1,26 @@
+'use strict';
+
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+var mapRouter = require('./routes/mapRouter.js');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// middleware to use for all requests
+// set the response headers
+app.use(function(req, res, next) {
+    // do logging
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  	res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Save-Data");
+  	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  	res.setHeader('Access-Control-Allow-Credentials', false);
+    next(); // make sure we go to the next routes and don't stop here
+});
 
 // choose whatever port we want to listen on
 var port = process.env.PORT || 8080;
@@ -8,6 +28,9 @@ var port = process.env.PORT || 8080;
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+// use our router middleware
+app.use('/', mapRouter);
 
 //Run the server
 app.listen(port, function() {
--- api/routes/mapRouter.js
+++ api/routes/mapRouter.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-module.exports = function() {
-	router.get('/', function(req, res) {
-		res.send("TEST WAS SECOND SUCCESSFUL");
-	});
-	router.get('/test', function(req, res) {
-		res.send("TEST WAS SUCCESSFUL");
-	});
-}
+
+router.get('/test', function(req, res) {
+	res.send("TEST WAS SUCCESSFUL");
+});
+
+module.exports = router;
