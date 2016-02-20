9ba377c726f22f14fe0b1277760f86f9a2fbc485
cspanda <robbin.xu@gmail.com>
2016-02-20 03:20:50 -0500

    Added new route to mapRouter.js

--- api/routes/mapRouter.js
+++ api/routes/mapRouter.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-
 module.exports = function() {
+	router.get('/', function(req, res) {
+		res.send("TEST WAS SECOND SUCCESSFUL");
+	});
 	router.get('/test', function(req, res) {
 		res.send("TEST WAS SUCCESSFUL");
 	});
